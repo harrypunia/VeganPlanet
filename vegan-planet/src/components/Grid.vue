061d753954d764fa64489594e75a82c4280c5054
harrypunia <harryrock67@gmail.com>
2021-03-14 23:20:28 -0400

    Created basic vue app with sass-loader and vue-router

---
+++ vegan-planet/src/components/Grid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Grid",
+    props: ["column", "center"]
+  }
+</script>
+
+<style lang="scss" scoped>@import "../resources/css/components/grid";</style>
---
+++ vegan-planet/src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <Grid>
+    Hey there
+  </Grid>
+</template>
+
+<script>
+
+  import Grid from "@/components/Grid";
+
+  export default {
+    name: "HomePage",
+    components: {Grid}
+  }
+
+</script>
+
+<style lang="scss" scoped>@import "../resources/css/pages/homePage";</style>
